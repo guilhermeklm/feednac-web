ae317c1abee5a0d9b6cfd7f3d21d31bbfe115f79
Guilherme Karam <gkaram@umov.me>
2024-06-12 17:29:05 -0300

    ajusta url api

--- src/components/ClassEvaluation.js
+++ src/components/ClassEvaluation.js
@@ -11,7 +11,7 @@ export default function ClassEvaluation() {
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/questions');
+        const response = await axios.get(`${process.env.REACT_APP_FEEDNAC_API}/questions`);
         console.log('Perguntas recebidas:', response.data.body.questions);
         setQuestions(response.data.body.questions);
       } catch (error) {
